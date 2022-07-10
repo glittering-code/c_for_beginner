a514d2a968409e6ae75bdfb6f79e5a8d5aa332a9
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-07-10 19:32:43 +0530

    Create session_4_binary_operator.C
    binary operator on integers in C

---
+++ session_4_binary_operator.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 4;
+    int num2 = 2;
+    int sum = num1+num2;
+    int difference = num1-num2;
+    int product = num1*num2;
+    int div = num1/num2;
+    int rem = num1%num2;
+    printf("sum = %d\n",sum);
+    printf("diff = %d\n",difference);
+    printf("multiplication = %d\n",product);
+    printf("div = %d\n",div);
+    printf("rem = %d\n",rem);
+   // printf("Hello World");
+
+    return 0;
+}
