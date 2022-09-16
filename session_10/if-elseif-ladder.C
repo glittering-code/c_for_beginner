dcac6d68a89a20ea74a6cfb22ded042206fcdec6
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-09-16 17:56:01 +0530

    Create if-elseif-ladder.C

---
+++ session_10/if-elseif-ladder.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+     int a,b;
+     printf("enter 2 numbers\n");
+     scanf("%d %d",&a,&b);
+     char operation;
+     printf("please enter the operator(+ , - , *, /)");
+     scanf(" %c",&operation);
+     if(operation == '+') {
+         int answer = a+b;
+         printf("answer is %d\n",answer);
+     } else if(operation == '-') {
+         int answer = a-b;
+         printf("answer is %d\n",answer);
+     } else if(operation == '*') {
+         int answer = a*b;
+         printf("answer is %d\n",answer);
+     } else if(operation == '/') {
+         float answer = a/b;
+         printf("answer is %f\n",answer);
+     } else {
+        printf("please enter the correct operator\n");    
+     }
+    //printf("Hello World");
+
+    return 0;
+}
