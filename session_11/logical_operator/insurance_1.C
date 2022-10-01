5b959d95606261f365029b05e1e6d20edb24e479
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-10-01 13:14:47 +0530

    Create insurance_1.C

---
+++ session_11/logical_operator/insurance_1.C
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int age;
+    char gender, marital_status;
+    printf("enter your age, gender(F/M) and marital_status respectively(M/U)\n");
+    scanf("%d %c %c",&age,&gender,&marital_status);
+    if(marital_status == 'M') {
+        printf("you are insured\n");
+    } else {
+        if(gender == 'M' && age > 30) {
+            printf("you are insured\n");
+        } else if(gender == 'F' && age > 25) {
+            printf("you are insured\n");
+        } else {
+            printf("you are not insured");
+        }
+    }
+    return 0;
+}
