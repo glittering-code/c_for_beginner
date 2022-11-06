102214d9f9f8d6b2297ceafa7861c3bf74c3b513
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-06 20:03:05 +0530

    Create counting.C

---
+++ session_16/counting.C
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    while(i <= 10) {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
