99e81269d3253bd63377232aff0c660b7dffc1c1
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-06 20:04:02 +0530

    Create even_numbers.C

---
+++ session_16/even_numbers.C
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+/* 2 4 6 8 10 12 ..... 90 92 94 96 98 100
+*/
+int main() {
+    int i = 2;
+    while(i <= 100) {
+        printf("%d\t",i);
+        i = i + 2;
+    }
+    return 0;
+}
