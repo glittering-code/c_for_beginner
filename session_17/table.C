eaef1b72d38213dcc013de3bf9d7821a722dccb2
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-16 20:38:26 +0530

    Create table.C

---
+++ session_17/table.C
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+// 21
+// 21 * 1 = 21
+// 21 * 2 = 42
+
+int main() {
+    int a;
+    printf("Which table you want to print\n");
+    scanf("%d",&a);
+    //int i = 1;
+    printf("===========Table of %d is as follow============\n",a);
+    /*while(i <= 10) {
+        printf("%d\n",a*i);
+        i++;
+    }*/
+    for(int i = 1 ; i <= 10 ; i++) {
+        printf("%d\n",a*i);
+    }
+    return 0;
+}
