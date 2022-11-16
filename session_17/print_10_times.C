58399bdb4879dd47607c7738d14b714b3cfe288d
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-16 20:39:22 +0530

    Create print_10_times.C

---
+++ session_17/print_10_times.C
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    /*int i = 1;
+    while(i <= 10) { // 1 2 3 4 5 6 7 8 9 10
+        
+    }
+    */
+    /*int i = 0; // initialization
+    while(i < 10) { // 0 1 2 3 4 5 6 7 8 9 
+        printf("welcome to glittering code\n");
+        i++; // increment
+    }*/
+    
+    for( int i = 0; i < 10; i++) {
+        printf("welcome to glittering code\n");
+    }
+    return 0;
+}
