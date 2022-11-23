de3aae8db30fc25c1ada9557e0dbe80fb9c27856
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-23 20:45:44 +0530

    Create count.C

---
+++ session_18/count.C
@@ -0,0 +1,25 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("How many numbers you want to enter\n");
+    int a;
+    scanf("%d",&a);
+    printf("enter %d numbers\n",a);
+    int pos = 0, neg = 0 , zero = 0;
+    for(int i = 0 ; i < a ; ++i) {
+        int var;
+        scanf("%d",&var);
+        if( var > 0) {
+            pos++;
+        } else if(var < 0) {
+            neg++;
+        } else {
+            zero++;
+        }
+    }
+    printf("count of +ve numbers = %d\n",pos);
+    printf("count of -ve numbers = %d\n",neg);
+    printf("count of zeros  = %d\n",zero);
+    return 0;
+}
