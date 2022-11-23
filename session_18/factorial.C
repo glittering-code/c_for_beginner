0c9176f9e270e73045e847d633b4d9b278300e12
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-11-23 20:44:45 +0530

    Create factorial.C

---
+++ session_18/factorial.C
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("enter a number whose factorial you want to calculate\n");
+    int a;
+    scanf("%d",&a);
+    int fact = 1;
+    /*
+    a = 5
+    i = 5, fact = 0
+    fact = 5, i = 4
+    fact = 20 i = 3
+    fact = 60 i = 2
+    fact = 120 i = 1
+    fact = 120 i = 0
+    */
+    for(int i = a; i >= 1 ; --i) { // a = 5
+        fact = fact *i;
+    }
+    if(a < 0) {
+        printf("factorial of -ve numbers is not allowed\n");
+    } else {
+        printf("factorial of number %d is %d\n",a,fact);
+    }
+    return 0;
+}
