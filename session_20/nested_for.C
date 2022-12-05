de9854be6d73addd52464491c6c1c6cc5dbdcb0c
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-12-05 20:39:32 +0530

    Create nested_for.C

---
+++ session_20/nested_for.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    for(int j = 0 ; j < 5 ; ++j) {
+        for(int i = 1 ; i <= 10 ; ++i) {
+            printf("%d ",i);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
