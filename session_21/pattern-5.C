a1ebb79b10cac5087361ac9272c3d9271b3ee66a
glittering-code <99531828+glittering-code@users.noreply.github.com>
2022-12-06 19:59:26 +0530

    Create pattern-5.C

---
+++ session_21/pattern-5.C
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    //printf("Hello world");
+    for(int i = 1 ; i <= 5 ; ++i) {
+        for(int j = 1 ; j <= i ; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
